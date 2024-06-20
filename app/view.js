cedfaaa536b8d690f9d9569e523caddd8d2bccc2
natalia <>
2024-06-19 20:49:28 -0700

    Minor adjustments to style | Set new game style for inputs with generated value

--- app/view.js
+++ app/view.js
@@ -4,6 +4,9 @@ function generateInput(value){
     let input = document.createElement('input');
     input.disabled = true;
     input.value = value;
+    input.type = 'number';
+    input.max = 9;
+    input.min = 1;
 
     return input;
 }
@@ -24,5 +27,16 @@ export function generateEmptyTable(){
 }
 
 export function generateGameTable(){
-
+    document.querySelectorAll('input').forEach( input => {
+        
+        let number = generateRandomNumber(); //test
+        
+        if(number - 1 > 3){
+            input.value = number;
+            input.setAttribute('disabled', '');
+        } else {
+            input.value = ''
+            input.removeAttribute('disabled');
+        }
+    })
 }
