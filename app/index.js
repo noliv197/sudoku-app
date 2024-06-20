a6bc9c0405b31db2ff17e3334f8a9986035ab78d
dieceva <dieceva96@gmail.com>
2024-06-19 20:35:08 -0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,5 +3,14 @@ import { generateEmptyTable, generateGameTable } from "./view.js";
 generateEmptyTable();
 
 document.querySelector('[data-btn="newGame"]').addEventListener('click', ()=>{
+    let table = document.querySelectorAll('input');
+    for(let input of table){
+        input.removeAttribute('disabled');
+        
+    }
+console.log(document.querySelectorAll('input'));
+
+
+
     generateGameTable();
 })
